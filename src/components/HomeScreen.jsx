bb589f5ffcf84203f410d3861de832731c798a9c
mhayeri <hayerimustafa@gmail.com>
2024-01-16 15:34:19 -0800

    Added components folder to clear up file structure

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/HomeScreen.css";
+
+import Navbar from "./Navbar";
+
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            <Navbar />
+
+            {/* Banner */}
+
+            {/* Rows - Movies/Shows */}
+        </div>
+    );
+}
+
+export default HomeScreen;
