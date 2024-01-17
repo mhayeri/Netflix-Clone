bfe8d8307b3cf352449e4d5db4e90eb49be42709
mhayeri <hayerimustafa@gmail.com>
2024-01-17 12:35:58 -0800

    Rendered new banner component to homescreen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "../styles/HomeScreen.css";
 
 import Navbar from "./Navbar";
+import Banner from "./Banner";
 
 function HomeScreen() {
     return (
         <div className="homeScreen">
             <Navbar />
 
-            {/* Banner */}
+            <Banner />
 
             {/* Rows - Movies/Shows */}
         </div>
