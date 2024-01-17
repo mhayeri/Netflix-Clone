8b66935bcaf7339fd7424b0b1b2359c894a5bd38
mhayeri <hayerimustafa@gmail.com>
2024-01-17 13:34:34 -0800

    Added truncate function to manage extremely long movie descriptions

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import "../styles/Banner.css";
 
 function Banner() {
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+    }
+
     return (
         <header
             className="banner"
@@ -19,7 +23,7 @@ function Banner() {
                     <button className="banner__button">My List</button>
                 </div>
                 <h1 className="banner__description">
-                    This is a test description
+                    {truncate(`This is a test description`, 150)}
                 </h1>
             </div>
             <div className="banner--fadeBottom" />
