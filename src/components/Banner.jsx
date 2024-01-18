15b43be9a5b419577344ead8d5f70d6926889380
mhayeri <hayerimustafa@gmail.com>
2024-01-18 13:06:00 -0800

    Added useEffect to fetch a random movie to populate the banner with

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../requests/axios";
+import requests from "../requests/requests";
 
 import "../styles/Banner.css";
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.ORIGINALS);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    }, []);
+
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n - 1) + "..." : string;
     }
@@ -12,18 +29,20 @@ function Banner() {
             className="banner"
             style={{
                 backgroundSize: "cover",
-                backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
                 backgroundPosition: "center center",
             }}
         >
             <div className="banner__contents">
-                <h1 className="banner__title">Movie Name</h1>
+                <h1 className="banner__title">
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
                 <h1 className="banner__description">
-                    {truncate(`This is a test description`, 150)}
+                    {truncate(`${movie?.overview}`, 175)}
                 </h1>
             </div>
             <div className="banner--fadeBottom" />
