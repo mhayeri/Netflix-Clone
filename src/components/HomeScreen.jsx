303ddf4d8393acabaf70dc185a9c5cabf4445cef
mhayeri <hayerimustafa@gmail.com>
2024-01-18 13:07:21 -0800

    Added movie row components to home screen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import "../styles/HomeScreen.css";
+import requests from "../requests/requests";
 
 import Navbar from "./Navbar";
 import Banner from "./Banner";
+import Row from "./Row";
 
 function HomeScreen() {
     return (
         <div className="homeScreen">
             <Navbar />
-
             <Banner />
-
-            {/* Rows - Movies/Shows */}
+            <Row title="Trending Now" url={requests.TRENDING} isLargeRow />;
+            <Row title="Netflix Originals" url={requests.ORIGINALS} />
+            <Row title="Top Rated" url={requests.TOP_RATED} />
+            <Row title="Action Movies" url={requests.ACTION} />
+            <Row title="Comedy Movies" url={requests.COMEDY} />
+            <Row title="Horror Movies" url={requests.HORROR} />
+            <Row title="Romance Movies" url={requests.ROMANCE} />
+            <Row title="Documentaries" url={requests.DOCUMENTARY} />
         </div>
     );
 }
