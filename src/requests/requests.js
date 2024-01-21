be964f4cb8c4edd929da310797922d6a37ca0be9
mhayeri <hayerimustafa@gmail.com>
2024-01-21 15:51:16 -0800

    Fixed env variable issue

--- src/requests/requests.js
+++ src/requests/requests.js
@@ -1,11 +1,5 @@
-const dotenv = require("dotenv");
-const path = require("path");
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
-const envPath = path.resolve(__dirname, "../../.env");
-
-dotenv.config({ path: envPath });
-
-const API_KEY = process.env.API_KEY;
 const requests = {
     TRENDING: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     ORIGINALS: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
