44a4db0dd667e9afb9fd84cbda64ddfd41d0573c
mhayeri <hayerimustafa@gmail.com>
2024-01-21 15:48:46 -0800

    added firebase configuration file

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "netflix-clone-a7913.firebaseapp.com",
+    projectId: "netflix-clone-a7913",
+    storageBucket: "netflix-clone-a7913.appspot.com",
+    messagingSenderId: "489265796843",
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
